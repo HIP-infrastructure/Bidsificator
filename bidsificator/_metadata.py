fee9b3af59a45d785f0e2bf759bba337bf80d4c9
Sipp Florian <Florian.Sipp@chuv.ch>
2025-09-02 08:40:53 +0200

    Use a py file for metadata info instead of pyproject file

---
+++ bidsificator/_metadata.py
@@ -0,0 +1,11 @@
+"""
+Single source of truth for application metadata.
+This file is used by both the AboutDialog and can be imported by pyproject.toml.
+"""
+
+__version__ = "1.5.3"
+__authors__ = ["Florian Sipp"]
+__author_email__ = "florian.sipp@chuv.ch"
+__copyright__ = "© 2025 CHUV - Lausanne University Hospital"
+__license__ = "Apache 2.0"
+__description__ = "A PyQt6 application for managing BIDS datasets"
